67d5cf05fc9984580ce02c8d194a7823f3d3f7e4
ajiteshprasad <ajitesh.prasad@gmail.com>
2020-06-01 03:09:27 +0000

    final commit

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -4,18 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import com.google.api.gax.rpc.NotFoundException;
-import com.google.api.gax.rpc.ServerStream;
-import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
-import com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
-import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
+
+import com.example.helloworld.beans.ModemData;
 import com.google.cloud.bigtable.data.v2.BigtableDataClient;
 import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
-import com.google.cloud.bigtable.data.v2.models.Query;
 import com.google.cloud.bigtable.data.v2.models.Row;
 import com.google.cloud.bigtable.data.v2.models.RowCell;
-import com.google.cloud.bigtable.data.v2.models.RowMutation;
 import java.io.IOException;
 
 @SpringBootApplication
@@ -24,35 +20,79 @@ public class HelloworldApplication {
 	@Value("${TARGET:World}")
 	String target;
 
-	private final String tableId = "demo2";
+	private String tableId;
 	private final String instanceId = "testdatabase";
 	private final String projectId = "mypocdata";
 	private BigtableDataClient dataClient;
 
 	@RestController
 	class HelloworldController {
-		@GetMapping("/")
-		String hello() {
+
+		@GetMapping("/{id}")
+		ModemData hello(@PathVariable("id") String id) {
+
+			ModemData modemdata = new ModemData();
+			tableId = System.getenv("tableid");
+
+			if (tableId == null) {
+				tableId = "demo2";
+			};
 
 			// Creates the settings to configure a bigtable data client.
 			BigtableDataSettings settings = BigtableDataSettings.newBuilder().setProjectId(projectId)
 					.setInstanceId(instanceId).build();
-			
-		    // Creates a bigtable data client.
-		    try {
+
+			// Creates a bigtable data client.
+			try {
 				dataClient = BigtableDataClient.create(settings);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		    String response = "start read";
-		    Row row = dataClient.readRow(tableId, "900001");
-		    response = response + row.getKey().toStringUtf8();
-		    for (RowCell cell : row.getCells()) {
-		    	response = response + cell.getQualifier().toStringUtf8() + cell.getValue().toStringUtf8();
-		    }
-            dataClient.close();
-			return response;
+
+			Row row = dataClient.readRow(tableId, id);
+
+			if ( row != null) {
+
+				for (RowCell cell : row.getCells()) {
+
+					switch(cell.getQualifier().toStringUtf8()) {
+
+					case "cal_timestamp_time":
+						modemdata.setCal_timestamp_time(cell.getValue().toStringUtf8());
+						break;
+
+					case "cell_id":
+						modemdata.setCell_id(cell.getValue().toStringUtf8());	
+						break;
+
+					case "subscriber_id":
+						modemdata.setSubscriber_id(cell.getValue().toStringUtf8());
+						break;
+
+					case "userequipment_imeisv":
+						modemdata.setUserequipment_imeisv(cell.getValue().toStringUtf8());
+						break;
+
+					case "cell_latitude":
+						modemdata.setCell_latitude(cell.getValue().toStringUtf8());
+						break;
+
+					case "cell_longitude":
+						modemdata.setCell_longitude(cell.getValue().toStringUtf8());
+						break;
+
+					case "subscriber_msisdn":
+						modemdata.setSubscriber_msisdn(cell.getValue().toStringUtf8());
+						break;
+
+					default:
+						//continue;
+					}
+				}
+			};
+			dataClient.close();
+			return modemdata;
 		}
 	}
 
---
+++ src/main/java/com/example/helloworld/beans/ModemData.java
@@ -0,0 +1,55 @@
+package com.example.helloworld.beans;
+
+public class ModemData {
+	
+
+    String cal_timestamp_time;
+    String cell_id ;
+    String subscriber_id;
+    String userequipment_imeisv;
+    String cell_latitude;
+    String cell_longitude;
+    String subscriber_msisdn;
+	public String getCal_timestamp_time() {
+		return cal_timestamp_time;
+	}
+	public void setCal_timestamp_time(String cal_timestamp_time) {
+		this.cal_timestamp_time = cal_timestamp_time;
+	}
+	public String getCell_id() {
+		return cell_id;
+	}
+	public void setCell_id(String cell_id) {
+		this.cell_id = cell_id;
+	}
+	public String getSubscriber_id() {
+		return subscriber_id;
+	}
+	public void setSubscriber_id(String subscriber_id) {
+		this.subscriber_id = subscriber_id;
+	}
+	public String getUserequipment_imeisv() {
+		return userequipment_imeisv;
+	}
+	public void setUserequipment_imeisv(String userequipment_imeisv) {
+		this.userequipment_imeisv = userequipment_imeisv;
+	}
+	public String getCell_latitude() {
+		return cell_latitude;
+	}
+	public void setCell_latitude(String cell_latitude) {
+		this.cell_latitude = cell_latitude;
+	}
+	public String getCell_longitude() {
+		return cell_longitude;
+	}
+	public void setCell_longitude(String cell_longitude) {
+		this.cell_longitude = cell_longitude;
+	}
+	public String getSubscriber_msisdn() {
+		return subscriber_msisdn;
+	}
+	public void setSubscriber_msisdn(String subscriber_msisdn) {
+		this.subscriber_msisdn = subscriber_msisdn;
+	}
+}
